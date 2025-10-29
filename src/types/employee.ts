df516388854d65f999cdf2b5106d89491a577a25
Antonny Njoroge <113513515+giichinga@users.noreply.github.com>
2025-10-29 11:41:19 +0300

    Add files via upload

---
+++ src/types/employee.ts
@@ -0,0 +1,31 @@
+export interface Employee {
+  id: string;
+  idNumber: string;
+  kraPin: string;
+  gender: 'male' | 'female';
+  firstName: string;
+  lastName: string;
+  email: string;
+  position: string;
+  salary: number;
+  startDate: string;
+  imageUrl: string;
+  profileImage?: File;
+  department: string;
+  employeenumber: string; 
+  phoneNumber: string; 
+}
+
+export interface PayrollCalculation {
+  grossPay: number;
+  tax: number;
+  sha: number;
+  nssf: number;
+  netPay: number;
+  customDeductions: CustomDeduction[];
+}
+
+export interface CustomDeduction {
+  name: string;
+  amount: number;
+}
